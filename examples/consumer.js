04d277fa840432ffb9f8a35ae0030c16add901c1
holyshared <holy.shared.design@gmail.com>
2016-02-29 12:28:17 +0900

    add example consumer

---
+++ examples/consumer.js
@@ -0,0 +1,13 @@
+var kue = require('kue');
+var consumer = kue.createQueue({
+  redis: {
+    host: '127.0.0.1',
+    port: 6379
+  }
+});
+
+consumer.process('example', function (job, done) {
+  job.log('ok');
+  done();
+});
+consumer.watchStuckJobs();
